662d67ad2dbdf56942190c1a30e76bb4b621659a
joacir <jometal@gmail.com>
2020-06-25 14:55:37 -0300

    Arquivos antigos excluidos

--- Config/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-$root = dirname(dirname(__FILE__));
-$ds = DIRECTORY_SEPARATOR;
-$vendorRootPath = $root . $ds . 'Vendor' . $ds . 'make-pdf' . $ds . 'lib' . $ds;
-App::build(array('Vendor' => array($vendorRootPath)));
-unset($ds, $root, $vendorRootPath); 
-
-App::import('Vendor', 'PdfDocument');
-App::import('Vendor', 'PdfReport');
